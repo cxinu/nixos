69d88fb84515b9f05e489c318b8e1fe22bfb06fd
cxinu <cxinu.main@protonmail.com>
2026-06-13 16:44:12 +0530

    sync mode: nvdia amd gpu

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -77,15 +77,27 @@
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     NIXOS_OZONE_WL = "1";
   };
+
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
+
+  services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
+
   hardware.nvidia = {
     modesetting.enable = true;
+    powerManagement.enable = false;
     open = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      sync.enable = true;
+
+      # lspci
+      amdgpuBusId = "PCI:5:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   # display manager
